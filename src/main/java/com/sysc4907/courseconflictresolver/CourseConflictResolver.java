bdefc3a4e5d29aff3df6d4d9e469644417d09dfe
malkh <mohammadalkhaledi2@gmail.com>
2023-10-04 10:37:33 -0400

    milestone 1 of our project complete: we have parsed the excel sheet, added the data into a database, and are able to query it.

--- src/main/java/com/sysc4907/courseconflictresolver/CourseConflictResolver.java
+++ src/main/java/com/sysc4907/courseconflictresolver/CourseConflictResolver.java
@@ -1,41 +1,61 @@
 package com.sysc4907.courseconflictresolver;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.poi.ss.usermodel.Row;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  *
  * @author Mohammad
  */
+@SpringBootApplication
 public class CourseConflictResolver {
 
+    @Bean
+    public CommandLineRunner demo(CourseRecordsRepository repo) {
+        return (args) -> {
+            System.out.println("Hello World!");
+            Parser parser = new Parser("src/main/resources/data_source.xlsx");
+
+            try {
+
+                var courseMap = parser.getColumnHeaderMap();
+
+                Iterator<Row> rowIterator = (parser.openFile()).iterator();
+
+                if (rowIterator.hasNext()) {
+                    rowIterator.next();
+                }
+
+                while (rowIterator.hasNext()) {
+                    Row row = rowIterator.next();
+
+                    var dataMap = parser.getDataMap(row);
+                    CourseRecords record = new CourseRecords();
+                    BeanUtils.populate(record, dataMap);
+                    repo.save(record);
+                    //System.out.println(dataMap);
+                }
+                CourseRecords record = repo.findByCRN("35871");
+                System.out.println(record.getCRN());
+
+            } catch (IOException | IllegalAccessException
+                    | InvocationTargetException ex) {
+                ex.printStackTrace();
+            }
+
+        };
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Parser parser = new Parser("src/main/resources/data_source.xlsx");
-        CourseRecords record = new CourseRecords();
-        try {
+        SpringApplication.run(CourseConflictResolver.class, args);
 
-            var courseMap = parser.getColumnHeaderMap();
-
-            Iterator<Row> rowIterator = (parser.openFile()).iterator();
-
-            if (rowIterator.hasNext()) {
-                rowIterator.next();
-            }
-
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-
-                var dataMap = parser.getDataMap(row);
-                
-                System.out.println(dataMap);
-            }
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
     }
 }
--- src/main/java/com/sysc4907/courseconflictresolver/CourseRecordsRepository.java
+++ src/main/java/com/sysc4907/courseconflictresolver/CourseRecordsRepository.java
@@ -1,11 +1,18 @@
 package com.sysc4907.courseconflictresolver;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  *
  * @author Mohammad
  */
-public interface CourseRecordsRepository extends CrudRepository<String, String>{
-    String findByCRN();
+public interface CourseRecordsRepository
+        extends CrudRepository<CourseRecords, Long>{
+    List<CourseRecords> findByBLDG(String BLDG);
+    CourseRecords findByCRN(String CRN);
+    List<CourseRecords> findBySTARTTIME(String STARTTIME);
+    List<CourseRecords> findByENDTIME(String ENDTIME);
+    List<CourseRecords> findBySTARTDATE(String STARTDATE);
+    List<CourseRecords> findByENDDATE(String ENDDATE);
 }
