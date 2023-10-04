a35d573e347d0279b329e97ddd7663eb9e872f22
malkh <mohammadalkhaledi2@gmail.com>
2023-10-04 02:22:37 -0400

    created parser and added column map reader method - reads first line of excel sheet and gets column names and their indices

--- src/main/java/com/sysc4907/courseconflictresolver/CourseConflictResolver.java
+++ src/main/java/com/sysc4907/courseconflictresolver/CourseConflictResolver.java
@@ -1,5 +1,9 @@
 package com.sysc4907.courseconflictresolver;
 
+import java.io.IOException;
+
+
+
 /**
  *
  * @author Mohammad
@@ -8,5 +12,13 @@ public class CourseConflictResolver {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Parser parser = new Parser("src/main/resources/data_source.xlsx");    
+        try {
+  
+            var courseMap = parser.getCourseMap();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
