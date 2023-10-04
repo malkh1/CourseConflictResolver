4c5cae660dab07b55815fdd4925dc1cdedfeff1d
malkh <mohammadalkhaledi2@gmail.com>
2023-10-03 21:07:42 -0400

    Add files via upload

---
+++ src/main/java/com/sysc4907/courseconflictresolver/CourseConflictResolver.java
@@ -0,0 +1,13 @@
+
+package com.sysc4907.courseconflictresolver;
+
+/**
+ *
+ * @author Mohammad
+ */
+public class CourseConflictResolver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
